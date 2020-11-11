d093ff4699878a74511240104ac384491c2e654c
Oliver Libutzki <oliver.libutzki@bmiag.de>
2020-11-11 13:38:33 +0100

    Initial commit

---
+++ src/test/java/de/libutzki/testcontainers/example/TestcontainersExampleTest.java
@@ -0,0 +1,23 @@
+package de.libutzki.testcontainers.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class TestcontainersExampleTest {
+
+	@Container
+	private final PostgreSQLContainer<?> postgresqlContainer = new PostgreSQLContainer<>( "postgres:12.4" )
+			.withDatabaseName( "foo" )
+			.withUsername( "foo" )
+			.withPassword( "secret" );
+
+	@Test
+	public void test( ) {
+		assertTrue( postgresqlContainer.isRunning( ) );
+	}
+}
